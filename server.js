6079f824ec05b84b071265c6ff43e44e9abb7063
Chris Carr <chriskcarrdev@gmail.com>
2021-01-23 13:49:15 +0100

    Modified signIn and register post requests

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
   users: [
@@ -37,7 +39,7 @@ app.post("/signin", (req, res) => {
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
   ) {
-    res.json("You logged in");
+    res.json("Success");
   } else {
     res.status(400).json("Error logging in");
   }
@@ -50,7 +52,6 @@ app.post("/register", (req, res) => {
     id: "125",
     name: name,
     email: email,
-    password: password,
     entries: 0,
     joined: new Date(),
   });
