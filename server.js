79d5969c9ecd7b6c552db1c771bac7eb3cb2e6cf
Chris Carr <chriskcarrdev@gmail.com>
2021-01-25 11:09:28 +0100

    Changes to Database connections

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-contoured-73654",
-    user: "krzysztof",
-    password: "",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
