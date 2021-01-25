e49ec3ce5e73a18dd74021b14f85584d4a1053ef
Chris Carr <chriskcarrdev@gmail.com>
2021-01-25 12:55:59 +0100

    Changes to db connection, added comments

--- server.js
+++ server.js
@@ -9,31 +9,55 @@ const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 
+// db connection - local host
+/*
 const db = knex({
   client: "pg",
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    host: "127.0.0.1",
+    user: "krzysztof",
+    password: "",
+    database: "smart-brain",
+  },
+});
+*/
+
+// db connection - remote heroku
+const db = knex({
+  // for connecting to PostgreSQL
+  client: "pg", // type of db
+  connection: {
+    connectionString: process.env.DATABASE_URL, // dynamic database value for heroku
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
-const app = express();
+//console.log(db.select('*').from('users')); //test connection to db is working
 
-app.use(bodyParser.json());
-app.use(cors());
+const app = express(); // express js server
+app.use(bodyParser.json()); // use 'app.use' as is middleware
+app.use(cors()); // use 'app.use' as is middleware
 
+//user database call
+//app.get('/', (req, res)=> { res.send(db.users) })
 app.get("/", (req, res) => {
   res.send("Started Server Side");
 });
+//sign-in call
 app.post("/signin", (req, res) => {
   signin.handleSignIn(req, res, db, bcrypt);
 });
+//register call
 app.post("/register", (req, res) => {
   register.handleRegister(req, res, db, bcrypt);
 });
+//profile call
 app.get("/profile/:id", (req, res) => {
   profile.handleProfileGet(req, res, db);
 });
+// image upload count call
 app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
@@ -42,5 +66,6 @@ app.post("/imageurl", (req, res) => {
 });
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log(`app is running on port: ${process.env.PORT}`);
+  //sets up server on dynamic port OR on port 4000.
+  console.log(`app is running on port: ${process.env.PORT}`); //server message on success.
 });
